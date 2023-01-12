e3004bd5962425af8228edc9dc38cd4fb437fa2a
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2023-01-12 01:51:06 +0100

    country_enum

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,5 +1,5 @@
--- Script that creates a table users following these requirements:
--- id, email, name, country(enumeration of US, CO and TN)
+-- SQL script that creates a table users with the following parameters
+-- id, email, name, country(enumeration of US, CO, and TN)
 CREATE TABLE IF NOT EXISTS users(
 	id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	email varchar(255) NOT NULL UNIQUE,
